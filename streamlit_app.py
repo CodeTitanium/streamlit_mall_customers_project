22748aeb2da2f9a47d666579beca767a73ce8fd1
BJoe25 <joseph.mbinji@strathmore.edu>
2024-10-06 12:30:27 +0300

    Added the start button

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,63 +5,70 @@ import streamlit as st
 model = pickle.load(open('model.pkl', 'rb'))
 scaler = pickle.load(open('scaler.sav', 'rb'))
 
-col0, col1, col2, col3, col4, col5 = st.columns(6)
-with col0:
-    st.write('')
-with col1:
-    st.write('')
-with col2:
-    st.write('CUSTOMERS')    
-with col3:
-    st.title('') 
-with col4:
-    st.write('')
-with col5:
-    st.write('')
+st.session_state.started = False
 
-col6, col7 = st.columns(2)
-    
-with col6:
-    st.markdown("<h6 style='text-align: left;'>A simple web app to segment/group mall customers</h6>", unsafe_allow_html=True)
-with col7:
-    st.write('')
-
-gen_list = ["Female", "Male"]
-
-gender = st.radio('Pick your gender', gen_list)
-if gender == "Male":
-    gender = int(1)
+if not st.session_state.started:
+    st.title("Welcome!")
+    if st.button("Let's get started"):
+        st.session_state.started = True
 else:
-    gender = int(0)
-
-age = st.slider('Pick your age', 18, 70)
-annual_income = st.slider('Pick your annual_salary in thousands of dollars $', 15, 137)
-spending_score = st.slider('Pick your spending score', 0, 100, 0, 1)
-
-# Feature Scaling
-user_input = np.array([[gender, age, annual_income, spending_score]])
-
-# Apply scaling on the combined 2D array
-user_input_scaled = scaler.transform(user_input)
-
-col10, col11, col12 = st.columns(3)
-with col10:
-    st.write('')    
-with col11:
-    predict_btn = st.button('Predict Customer_Segment')
-with col12:
-    st.write('')
-
-if(predict_btn):
-    inp1 = float(user_input_scaled[0][0])
-    inp2 = float(user_input_scaled[0][1])
-    inp3 = float(user_input_scaled[0][2])
-    inp4 = float(user_input_scaled[0][3])
-    X = [inp1, inp2, inp3, inp4]
-    customer_group = model.predict([X])
-    col15, col16 = st.columns(2)
-    with col15:
-        st.write('The 5 possible customer groups are: 0, 1, 2, 3, 4')    
-    with col16:
-        st.text(f"Estimated group: {customer_group}")
+    col0, col1, col2, col3, col4, col5 = st.columns(6)
+    with col0:
+        st.write('')
+    with col1:
+        st.write('')
+    with col2:
+        st.write('CUSTOMERS')    
+    with col3:
+        st.title('') 
+    with col4:
+        st.write('')
+    with col5:
+        st.write('')
+    
+    col6, col7 = st.columns(2)
+        
+    with col6:
+        st.markdown("<h6 style='text-align: left;'>A simple web app to segment/group mall customers</h6>", unsafe_allow_html=True)
+    with col7:
+        st.write('')
+    
+    gen_list = ["Female", "Male"]
+    
+    gender = st.radio('Pick your gender', gen_list)
+    if gender == "Male":
+        gender = int(1)
+    else:
+        gender = int(0)
+    
+    age = st.slider('Pick your age', 18, 70)
+    annual_income = st.slider('Pick your annual_salary in thousands of dollars $', 15, 137)
+    spending_score = st.slider('Pick your spending score', 0, 100, 0, 1)
+    
+    # Feature Scaling
+    user_input = np.array([[gender, age, annual_income, spending_score]])
+    
+    # Apply scaling on the combined 2D array
+    user_input_scaled = scaler.transform(user_input)
+    
+    col10, col11, col12 = st.columns(3)
+    with col10:
+        st.write('')    
+    with col11:
+        predict_btn = st.button('Predict Customer_Segment')
+    with col12:
+        st.write('')
+    
+    if(predict_btn):
+        inp1 = float(user_input_scaled[0][0])
+        inp2 = float(user_input_scaled[0][1])
+        inp3 = float(user_input_scaled[0][2])
+        inp4 = float(user_input_scaled[0][3])
+        X = [inp1, inp2, inp3, inp4]
+        customer_group = model.predict([X])
+        col15, col16 = st.columns(2)
+        with col15:
+            st.write('The 5 possible customer groups are: 0, 1, 2, 3, 4')    
+        with col16:
+            st.text(f"Estimated group: {customer_group}")
         
